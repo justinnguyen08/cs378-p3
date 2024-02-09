9ea0f2f73b76fc39fa400600b50d46edc5075a3c
Justin Nguyen <112197989+justinnguyen08@users.noreply.github.com>
2024-02-09 14:22:18 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import MenuItem from './components/MenuItem';
+import Descriptor from "./components/Descriptor";
 
-// import 'bootstrap/dist/css/bootstrap.min.css'; // This imports bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
+import 'bootstrap/dist/css/bootstrap.min.css'; // This imports bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
 
 // Menu data. An array of objects where each object represents a menu item. Each menu item has an id, title, description, image name, and price.
 // You can use the image name to get the image from the images folder.
@@ -82,10 +83,16 @@ const menuItems = [
 function App() {
   return (
     <div>
-      <h1>Menu</h1>
+      <Descriptor />
       <div className="menu">
-        {/* Display menu items dynamicaly here by iterating over the provided menuItems */}
-        <MenuItem title={menuItems[0].title} /> {/* Example for how to use a component */}
+        {menuItems.map(item => (
+          <MenuItem 
+          key = {item.id} 
+          title = {item.title} 
+          description = {item.description}
+          imageName = {item.imageName}
+          price = {item.price}/>
+        ))}
       </div>
     </div>
   );
---
+++ src/components/Descriptor.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Descriptor = () => {
+    return (
+        <div className="descriptor">
+            <h1>
+                <img 
+                    src="https://media.istockphoto.com/id/929930644/vector/chinese-restaurant-sign.jpg?s=612x612&w=0&k=20&c=ffklYmCvszatPLpCLH4NEnLcdGsCZLAc9lv3MuzqtS8=" 
+                    width="200" 
+                    height="200" 
+                    alt="Chinese man holding chopsticks and takeout lo mein"
+                />
+                Chef Wang's
+            </h1>
+            <br />
+            <h2 className="decorative">The Pinnacle of Authentic Chinese Cuisine</h2>
+            <h3>Fine Food, Fine Ingredients, Fine Experience</h3>
+            <br />
+        </div>
+    );
+};
+
+export default Descriptor;
